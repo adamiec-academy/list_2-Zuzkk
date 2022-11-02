34d30967c1a748f64c5860657ab9e3061d32721f
Zuzia <115666479+Zuzkk@users.noreply.github.com>
2022-11-02 13:54:40 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,21 @@
 def remove_parentheses(text):
-    return text
+    result = ""
+    delate_spacebar = False
+    is_inside = False
+    for letter in text:
+        if letter == "(":
+            is_inside = True
+        elif letter == ")":
+            is_inside = False
+            delate_spacebar = True
+        elif delate_spacebar:
+            result = result
+            delate_spacebar = False
+        elif not delate_spacebar and not is_inside:
+            result += letter
+    return result
+
+
+text = "(Nie) jest tak źle"
+print(remove_parentheses(text))
+

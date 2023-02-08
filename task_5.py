9b875706aa4a8c15472ffbbb87b8c88be648ce0f
Zuzia <115666479+Zuzkk@users.noreply.github.com>
2023-02-08 17:13:05 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -14,18 +14,4 @@ def cipher(text, shift):
 
 
 def decipher(text, shift):
-    result = ""
-    for letter in text:
-        if letter == " ":
-            result += letter
-        elif letter.isupper() and ord(letter) - shift < ord('A'):
-            result += chr(ord(letter) - shift + 26)
-        elif not letter.isupper() and ord(letter) - shift < ord('a'):
-            result += chr(ord(letter) - shift + 26)
-        else:
-            result += chr(ord(letter) - shift)
-    return result
-
-
-print(cipher("Imperator", 1))
-print(decipher("Jnqfsbups", 1))
+    return cipher(text, -shift)
